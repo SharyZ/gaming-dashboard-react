c6baefd16add6145edf7b19156e3f26fbd794ea3
SharyZ <onshariiit@gmail.com>
2022-03-14 20:13:17 +0500

    setting up tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,29 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Rubik', 'sans-serif'],
+      },
+      colors: {
+        heading: '#22283C',
+        primary: '#fc8b40',
+      },
+      boxShadow: {
+        'base': '0 6px 24px rgba(252, 139, 64, .2)',
+        'gray': '0 6px 24px rgba(183, 183, 183, .4)',
+      },
+      gridTemplateColumns: {
+        'popular-games': 'repeat(auto-fit, minmax(200px, 1fr))',
+        'released-games': 'repeat(auto-fit, minmax(254px, 1fr))',
+      },
+      container: {
+        center: true,
+        padding: '10px',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+  ],
 }
