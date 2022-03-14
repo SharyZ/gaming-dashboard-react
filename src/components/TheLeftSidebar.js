0691b06f89d1375b98868dc558cd41f4eb31b0b9
SharyZ <onshariiit@gmail.com>
2022-03-14 21:27:06 +0500

    use heroicons library instead svg icons

--- src/components/TheLeftSidebar.js
+++ src/components/TheLeftSidebar.js
@@ -1,3 +1,5 @@
+import { MenuAlt2Icon } from "@heroicons/react/outline"
+
 import { TheNav } from "./TheNav"
 import { TheFooter } from "./TheFooter"
 
@@ -7,20 +9,7 @@ export const TheLeftSidebar = () => {
       className="sticky top-0 left-0 hidden h-screen w-[130px] flex-col justify-between bg-white p-[40px] md:flex"
     >
       <button className="inline-block rounded p-[10px] focus:ring-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth="2"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M4 6h16M4 12h16M4 18h7"
-          />
-        </svg>
+        <MenuAlt2Icon className="w-6 h-6" />
       </button>
       <TheNav />
       <TheFooter />
