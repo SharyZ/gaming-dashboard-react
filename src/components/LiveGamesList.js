ccfa7d65791cb965d833f4e421e3361fe07e4032
SharyZ <onshariiit@gmail.com>
2022-03-15 22:03:28 +0500

    add "not found" element if data not provided

--- src/components/LiveGamesList.js
+++ src/components/LiveGamesList.js
@@ -3,7 +3,9 @@ import { LiveGamesListItem } from "./LiveGamesListItem"
 export const LiveGamesList = ({ liveGames }) => {
   return (
     <div className="flex flex-col space-y-[15px]">
-      {liveGames.map((liveGame) => <LiveGamesListItem key={liveGame.id} {...liveGame} />)}
+      {liveGames.length ? (
+        liveGames.map((liveGame) => <LiveGamesListItem key={liveGame.id} {...liveGame} />)
+      ) : <p>Games not found.</p>}
     </div>
   )
 }
--- src/components/ReleasedGamesList.js
+++ src/components/ReleasedGamesList.js
@@ -3,9 +3,11 @@ import { ReleasedGamesListItem } from "./ReleasedGamesListItem"
 export const ReleasedGamesList = ({ releasedGames }) => {
   return (
     <div className="grid grid-cols-released-games gap-[20px]">
-      {releasedGames.map((releasedGame) => (
-        <ReleasedGamesListItem key={releasedGame.id} {...releasedGame} />
-      ))}
+      {releasedGames.length ? (
+        releasedGames.map((releasedGame) => (
+          <ReleasedGamesListItem key={releasedGame.id} {...releasedGame} />
+        ))
+      ) : <p>Games not found.</p>}
     </div>
   )
 }
